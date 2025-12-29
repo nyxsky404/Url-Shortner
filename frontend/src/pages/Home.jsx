9bdd4002a6418da2d4e7f5cf2ea43a643fd20b4e
Sumit Kumar <sumit76231@gmail.com>
2025-12-30 00:00:23 +0530

    frontend update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import './Home.css'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000'
 
 function Home({ navigateTo }) {
   const [url, setUrl] = useState('')
